9ce4d32f1da5e7afac41b609e088e0dbf08b08bd
eleu9da <123483719+eleu9da@users.noreply.github.com>
2023-01-26 17:36:32 -0300

    Add files via upload
    Hamqth replacement for sh/qrz in Dirk Koopman's G1TLH DXSpider
    
    install:
    download  WWW::Mechanize from CPAN
    copy thew file under /home/sysop/spider/local_cmd/show/
    Edit the file and change the line $url = "http://www.hamqth.com/xml.php?u=YOURCALL&p=YOURPASS"; with your real hamqth user and password, don't forget to save the changes
    
    use it, from your DXSpider prompt do sh/hqth lu9da and see my info!
    
    Enjoy!

---
+++ hqth.pl
@@ -0,0 +1,113 @@
+#
+# Query the QRZ Database server for a callsign
+#
+# from an idea by Steve Franke K9AN and information from Angel EA7WA
+#
+# Copyright (c) 2001 Dirk Koopman G1TLH
+#
+# modified on 5/13/2009 Ricardo Suarez LU9DA to work with the hamqth web page.
+#
+# need for WWW:Mechanize ! download it from CPAN
+#
+# working under test on lu9da-6 cluster ( lu9da.no-ip.org 9000)
+#
+
+use WWW::Mechanize;
+
+
+my ($self, $line) = @_;
+my @list = split /\s+/, $line;		      # generate a list of callsigns
+my $l;
+my $call = $self->call;
+my @out;
+my $url;
+my $page;
+my $count;
+my $linea;
+my $indx;
+my $session_id;
+
+return (1, $self->msg('e24')) unless $Internet::allow;
+return (1, "SHOW/HQTH <callsign>, e.g. SH/HQTH lu9da") unless @list;
+
+$url = "http://www.hamqth.com/xml.php?u=YOURCALL&p=YOURPASS";
+
+my $mechanize = WWW::Mechanize->new(autocheck => 1);
+$mechanize->agent_alias('Windows Mozilla');
+
+
+#----------------------
+
+$mechanize->get($url);
+
+$page = $mechanize->content;
+
+my @tabla = split('\n', $page);
+
+
+foreach $linea (@tabla) {
+
+	if ($linea =~ m/<session_id>/) {
+
+	    $linea =~ s/<session_id>//;
+
+	    $linea =~ s/<\/session_id>//;
+
+	    $session_id = $linea;
+
+	    $session_id =~ s/^\^s+//;
+
+	    }
+
+}
+#-----------
+
+
+$session_id =~ s/^\s+//;
+
+
+$url = "http://www.hamqth.com/xml.php?id=$session_id&callsign=@list&prg=hqth.pl";
+
+$mechanize->get($url);
+
+$page = $mechanize->content;
+
+my @tabla = split('\n', $page);
+
+push @out," ";
+
+push @out,"HAMQTH search for @list:";
+
+push @out," ";
+
+foreach $linea (@tabla) {
+
+	if ($linea =~ m/<HamQTH/) {
+	    $linea="";
+	    }
+		
+	
+		$linea =~ s/<\?xml version=\"1.0\"\?>//;
+		$linea =~ s/<\/HamQTH>//;
+		$linea =~ s/<search>//;
+		$linea =~ s/<\/search>//;
+		$linea =~ s/<session>//;
+		$linea =~ s/<\/session>//;
+#
+		my $pos=rindex($linea,"</");
+		$linea=substr($linea,0,$pos);
+
+		$linea =~ s/<//;
+		$linea =~ s/>/ : /;
+		
+		push @out, $linea;
+
+		}
+
+
+
+
+return (1, @out);
+
+
+
